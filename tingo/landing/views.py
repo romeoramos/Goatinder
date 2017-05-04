7055f61ca8d8c5307c41aaf22b5f0f9dc123c2d8
Oscar Romeo Ramos Molina <RomeoRamos@MacBook-Pro-de-Oscar-2.local>
2017-05-03 19:24:44 -0500

    SIXTH COMMIT

--- tingo/landing/views.py
+++ tingo/landing/views.py
@@ -1,6 +1,47 @@
 from django.shortcuts import render
 
 # Create your views here.
+from modules.users.models import User
+from django.contrib.auth import authenticate,logout as salir,login as iniciar
+from django.http import HttpResponse
+from django.conf import settings
+from .models import Images
+# Create your views here.
 
 def index(request):
-    return render(request, 'landing/index.html')
+    return render(request, 'landing/index.html',{'request':request,
+    'ImageProfile':ImageProfile})
+
+
+def login(request):
+    form = LoginForm(request.POST or None)
+    if request.method == "POST":
+        if form.is_valid():
+            user = authenticate(
+                email=form.cleaned_data['email'],
+                password=form.cleaned_data['password']
+                )
+            if user is not None:
+                iniciar(request,user)
+                return redirect('landing:index')
+            else:
+                return HttpResponse("Usuario no encontrado")
+
+    return render(request,'landing/login.html', {"login":form})
+
+
+def signup(request):
+    form = SignupForm(request.POST or None)
+    if request.method == 'POST':
+        print(form.is_valid())
+        if form.is_valid():
+            form.cleaned_data.pop('confirm_password', None)
+            user = User.objects.create_user(**form.cleaned_data)
+            return redirect("landing:index")
+
+    return render(request,'landing/sign.html',{'sign':form})
+
+
+def logout(request):
+    salir(request)
+    return redirect("landing:index")
